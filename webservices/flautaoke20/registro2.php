8f02c3771f512b0d4b0b39f38c69154b62bcc297
arkoscr <tutordejava@gmail.com>
2019-09-17 14:02:04 -0600

    Ajustes en registro

--- webservices/flautaoke20/registro2.php
+++ webservices/flautaoke20/registro2.php
@@ -3,12 +3,14 @@
 	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 	header("Content-Type: text/html; charset=utf-8");
-	
 	require 'funcs/conexion.php';
 	require 'funcs/funcs.php';
 	$JSONData = file_get_contents("php://input");
-	$dataObject = json_decode($JSONData);		
-		
+	$dataObject = json_decode($JSONData);
+
+	$errors = array();
+	$mjs = "";
+	
 		$nombre = utf8_decode ($dataObject-> nombre);
 		$apellido1 = utf8_decode ( $dataObject-> apellido1);
 		$apellido2 = utf8_decode ( $dataObject-> apellido2);
@@ -17,59 +19,64 @@
 		$provincia = utf8_decode ($dataObject-> provincia);
 		$sexo = $dataObject-> sexo;
 		$fechaNacimiento = $dataObject-> fechaNacimiento;
-		$clave = $dataObject-> clave;
-		$confirmaClave = $dataObject-> confirmaClave;
+		$password = $dataObject-> clave;
+		$con_password = $dataObject-> confirmaClave;
 		$activo = 1;
-		$tipoUsuario = 1;
-	
-	//Se crea el objeto persona para enviarlo al cliente en caso de resgistro satisfactorio o error			
-	$persona = new stdClass();
-	$persona->usuario = "nulo";
-	$persona->error = 1;
-	$persona->mensaje="nulo";
-
-	//$claveEncriptada =  $clave;
-	//$token = "456defr";
-
-	$claveEncriptada = hashPassword($clave);
-	$token = generateToken();
-
-	$tipoUsuario = 4;
-	$activo = 1;
-
-
-	// Create connection
-		$conn = conectarDB();
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-			$persona->mensaje = "Error de conexión";
-			$persona->error = 1;
-			echo json_encode( $persona );
+		$tipoUsuario = 4;
+		if(isNull($nombre, $usuario, $password, $con_password))
+		{
+			$errors[] = "Debe llenar todos los campos";
+			
 		}
 
-		$sql = "INSERT INTO usuarios (usuario, claveEncriptada, nombre, apellido1, apellido2, pais, provincia, fechaNacimiento, sexo, token, tipoUsuario, activo)
-		VALUES ('$usuario', '$claveEncriptada', '$nombre', '$apellido1', '$apellido2', '$pais', '$provincia', '$fechaNacimiento', '$sexo', '$token', '$tipoUsuario', '$activo')";
-
-		if ($conn->query($sql) === TRUE) {
-			$persona->mensaje = "Registro insertado de forma satisfacotria";
-			$persona->error = 0;
-			$persona->usuario = $usuario;
-			echo json_encode( $persona );
-		} else {
-			$persona->mensaje = "Error al insertar registro";
-			$persona->error = 1;
-			echo json_encode( $persona );
+ 		if(!validaPassword($password, $con_password))
+		{
+			$errors[] = "Las contraseÃ±as no coinciden";
+			
 		}
 
-		$conn->close();
-		
+		if(usuarioExiste($usuario))
+		{
+			$errors[] = "El nombre de usuario $usuario ya existe";
+			
+		}
+
+		if(count($errors) == 0)
+		{
+
+
+				$pass_hash = hashPassword($password);
+				$token = generateToken();
+
+				$registro = registraUsuario($usuario, $pass_hash, $nombre, $apellido1, $apellido2, $pais, $provincia, $fechaNacimiento, $sexo, $token, $tipoUsuario, $activo);
+
+				if($registro > 0 )
+				{
+
+					$mjs="Felicitaciones!, su cuenta ha sido registrada";
+					// echo "<br><a href='index.php' >Iniciar Sesion</a>";
+					echo json_encode(array('error'=>false,'usuario'=>$usuario,'mensaje'=>$mjs));
+					exit;
+
+					} else {
+						$errors[] = "Error al crear la cuenta";
+						echo json_encode(array('error'=>true, 'mensaje'=>$errors ));
+					}
 
 
 
+		}
 
+	// }
 
-
-
+	function calculaedad($fechanacimiento){
+	  list($ano,$mes,$dia) = explode("-",$fechanacimiento);
+	  $ano_diferencia  = date("Y") - $ano;
+	  $mes_diferencia = date("m") - $mes;
+	  $dia_diferencia   = date("d") - $dia;
+	  if ($dia_diferencia < 0 || $mes_diferencia < 0)
+	    $ano_diferencia--;
+	  return $ano_diferencia;
+	}
 
 ?>
